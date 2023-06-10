cdb672e89ef73aac07165f357759c4a7773f4f32
rteixeiragarcia <rteixeiragarcia@gmail.com>
2023-06-10 11:27:58 -0300

    Solve the exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
 
+    if (num2 > num1) {
+        for (i = num1; i <= num2; i++) {
+            sum += i;
+        }
+    } else if (num2 < num1) {
+        for (i = num2; i <= num1; i++) {
+            sum += i;
+        }
+    }
+
+    if (typeof num1 !== "number" || typeof num2 !== "number" || num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
