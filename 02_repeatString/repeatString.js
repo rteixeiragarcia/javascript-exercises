b97289175e0b2456dc0b1361f4af82e589011fc2
rteixeiragarcia <rteixeiragarcia@gmail.com>
2023-06-10 10:40:28 -0300

    Solve the exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let finalString = "";
 
+    if (num < 0) {
+        return "ERROR";
+    }
+
+    for (let i = 0; i < num; i++) {
+        finalString = finalString.concat(string)
+    }
+
+    return finalString;
 };
 
 // Do not edit below this line
