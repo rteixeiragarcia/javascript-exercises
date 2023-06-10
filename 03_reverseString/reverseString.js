fe45062a08c36bf505bb2a43be1ab77212d2c021
rteixeiragarcia <rteixeiragarcia@gmail.com>
2023-06-10 10:57:57 -0300

    Solve the exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverseString = "";
 
+    for (let i = string.length - 1; i > -1; i--) {
+        reverseString = reverseString.concat(string.charAt(i));
+    }
+
+    return reverseString;
 };
 
 // Do not edit below this line
