53d7e0e9c81dbad1c1af8ac9c5b7fc328130076f
rteixeiragarcia <rteixeiragarcia@gmail.com>
2023-06-15 20:33:15 -0300

    Solve the exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
 const add = function() {
-	
+	let array = Array.from(arguments);
+
+  return array.reduce(function(sum, current) {
+    return sum += current;
+  }, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let array = Array.from(arguments);
+
+  return array.reduce(function(sub, current) {
+    let subTotal = -1 * sub - current;
+    return subTotal;
+  }, 0)
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	 return array.reduce(function(sum, current) {
+    return sum + current;
+  }, 0)
 };
 
 const multiply = function() {
+  let array = Array.from(arguments);
 
+  return array.reduce(function(multi, current) {
+    return multi *= current;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let array = [];
+
+  for (let i = 1; i <= num; i++) {
+    array.push(i);
+  }
+
+  return array.reduce(function(factorial, current) {
+    return factorial *= current;
+  }, 1)
 };
 
 // Do not edit below this line
