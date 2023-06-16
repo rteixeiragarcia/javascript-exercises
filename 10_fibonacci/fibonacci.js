42db1244bffc394b44f833d8aac71f5aa6d68953
rteixeiragarcia <rteixeiragarcia@gmail.com>
2023-06-15 22:05:58 -0300

    Solve the exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+    let array = [1, 1];
 
+    for (let i = 2; i < value; i++) {
+        array[i] = array[i-2] + array[i-1];
+    }
+
+    if (value <= 0) {
+        return "OOPS";
+    } else {
+        return array[array.length - 1];
+    };
 };
 
 // Do not edit below this line
