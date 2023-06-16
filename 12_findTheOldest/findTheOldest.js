dfa16dee136177f622a56805a7c9592d30fd4e48
rteixeiragarcia <rteixeiragarcia@gmail.com>
2023-06-15 23:18:13 -0300

    Solve the exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,40 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let ages = people.map(item => {
+        if ("yearOfDeath" in item) {
+            return item.yearOfDeath - item.yearOfBirth;
+        } else {
+            return 2023 - item.yearOfBirth;
+        }
+    });
 
+    let index = ages.findIndex(age => {
+        let oldest = Math.max.apply(0, ages);
+
+        return age === oldest;
+    });
+
+    return people[index];
 };
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+  findTheOldest(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;
