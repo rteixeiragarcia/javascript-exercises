693eb66c0a4764f667983f9d191dc4d2e41f4f44
rteixeiragarcia <rteixeiragarcia@gmail.com>
2023-06-15 21:50:16 -0300

    Solve the exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let arrayString = Array.from(string);
 
+    let arrayStringClean = arrayString.filter(function (item){
+        return /[a-zA-Z0-9]/.test(item);
+    })
+
+    let arrayStringClean2 = arrayString.filter(function (item){
+        return /[a-zA-Z0-9]/.test(item);
+    })
+
+    arrayStringClean2.reverse();
+
+    if (arrayStringClean2.toString().toLowerCase() == arrayStringClean.toString().toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+palindromes('A car, a man, a maraca.')
 
 // Do not edit below this line
 module.exports = palindromes;
